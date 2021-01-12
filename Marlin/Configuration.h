e506e5c86027c84f773095a19092235083c8f570
Master Bo <smscat@gmail.com>
2021-01-12 15:11:05 +0200

    Bltouch should work

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -105,13 +105,13 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT -1
+#define SERIAL_PORT 2
 
 /**
  * Select a secondary serial port on the board to use for communication with the host.
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 2
+#define SERIAL_PORT_2 -1
 
 /**
  * This setting determines the communication speed of the printer.
